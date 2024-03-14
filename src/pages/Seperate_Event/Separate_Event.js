93fbccf318f6fc127a85671ede4e04df20b24abf
sanskarsinghiitbhu <sanskar.singh.eee21@itbhu.ac.in>
2024-03-15 00:43:12 +0530

    Updated Recognizance landing page

--- src/pages/Seperate_Event/Separate_Event.js
+++ src/pages/Seperate_Event/Separate_Event.js
@@ -187,7 +187,10 @@ const Separate_Event = ({ data }) => {
         style={{ backgroundImage: `url(${data.imgpath})` }}
       >
         <div className=" min-h-screen w-full md:w-3/5 lg:w-[45%] bg-black md:opacity-[0.8] opacity-[0.7] text-white p-[3rem]">
-          <h1 className="font-bold text-3xl md:text-2xl mt-10 mb-4 font-[Poppins]">
+          {data.title === "Recognizance" && (
+            <div dangerouslySetInnerHTML={{ __html: data.sponsorInfo }} />
+          )}
+          <h1 className="font-bold text-3xl md:text-2xl mt-4 mb-2 font-[Poppins]">
             {data.title}
           </h1>
           <h2 className="md:text-xl xl:text-2xl mb-4 text-[#29ffff] font-[Poppins] text-lg">
@@ -196,18 +199,20 @@ const Separate_Event = ({ data }) => {
           <p className="md:text-md xl:text-lg text-justify font-[Nunito] text-md mb-4">
             {data.eventInfo}
           </p>
-          <h3 className="md:text-md xl:text-lg text-justify font-[Nunito] text-md">
-            Participants : <span>{eventName}</span>
-          </h3>
-
-          {/* {result.team_event && <div className="font-[Nunito] mt-[1em] font-bold"> Team Event</div> }
-          {!result.team_event && <div className="font-[Nunito] mt-[1em] font-bold">  Solo Event</div> } */}
+          <div className="flex justify-between">
+  <h3 className="md:text-md xl:text-lg text-justify font-[Nunito] text-md">
+    Participants : <span>{eventName}</span>
+  </h3>
+  {data.title === "Recognizance" && (
+    <div style={{ fontSize: "1.2rem", fontWeight: "normal", color : "#C9CDD3"}} dangerouslySetInnerHTML={{ __html: data.prize }} />
+  )}
+</div>
           {localStorage.getItem("loginData") ? (
             <Link onClick={register}>
               <button
                 className="mt-8 border-2 border-[white] px-10 py-3 rounded-3xl hover:bg-[#d5d8d8] hover:text-black font-Catamaran
                   "
-              >
+                  >
                 Register
               </button>
             </Link>
@@ -216,7 +221,7 @@ const Separate_Event = ({ data }) => {
               <button
                 className="mt-8 border-2 border-[white] px-10 py-3 rounded-3xl hover:bg-[#d5d8d8] hover:text-black font-Catamaran
                   "
-              >
+                  >
                 Register
               </button>
             </Link>
