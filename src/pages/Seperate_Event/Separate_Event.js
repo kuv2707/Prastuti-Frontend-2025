3c4e93e2753f2617760f72f1a3f22292f2ec50b1
sanskarsinghiitbhu <sanskar.singh.eee21@itbhu.ac.in>
2024-03-15 00:57:19 +0530

    Updates Recognizance landing page

--- src/pages/Seperate_Event/Separate_Event.js
+++ src/pages/Seperate_Event/Separate_Event.js
@@ -160,7 +160,7 @@ const Separate_Event = ({ data }) => {
     if (result.team_event) {
       handleModal(true);
       // let teamName = prompt(
-      //   "Please enter your team name that you have created in profil epage",
+      //   "Please enter your team name that you have created in the profile page",
       //   "team name"
       // );
       // findingteam(teamName);
